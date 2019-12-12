552131f20aac211416d0790fd3b2d988ce657d98
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-12-12 15:25:42 +0600

    Закончил

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 import Countries from "./Components/Countries/Countries";
 
+
 class App extends Component{
     render() {
         return(
             <div className="App">
                 <Countries/>
+
             </div>
         )
     }
